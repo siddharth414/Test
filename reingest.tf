fecd057dcce58d34078501b3b5f555751c5eac0f
Siddharth Mag <90581558+siddharth414@users.noreply.github.com>
2025-07-16 11:28:54 -0400

    Delete reingest.tf

--- reingest.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_cloudwatch_log_group" "reingest_logs" {
-  name = "/aws/lambda/${aws_lambda_function.firehose_lambda_reingest.function_name}"
-  retention_in_days = var.cloudwatch_log_retention
-  tags = var.tags
-}
